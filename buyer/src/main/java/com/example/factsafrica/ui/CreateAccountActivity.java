f5f4e7f845919b74ec6c46024d37d0da2a0b5ac9
ShadrackAdwera <adweshshaddie@gmail.com>
2020-03-04 11:11:45 +0300

    Initial commit: Boiler Plate Code

---
+++ buyer/src/main/java/com/example/factsafrica/ui/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.example.factsafrica.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.factsafrica.R;
+
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
---
+++ buyer/src/main/java/com/example/factsafrica/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.factsafrica.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.factsafrica.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
