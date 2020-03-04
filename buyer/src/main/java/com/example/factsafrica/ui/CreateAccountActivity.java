cbe694aa8cdefc284cb2fea86688ae7a7bd7961c
Hammertonmutuku <lthamma3@gmail.com>
2020-03-04 15:57:16 +0300

    implimented authentication

--- buyer/src/main/java/com/example/factsafrica/ui/CreateAccountActivity.java
+++ buyer/src/main/java/com/example/factsafrica/ui/CreateAccountActivity.java
@@ -1,15 +1,23 @@
 package com.example.factsafrica.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.factsafrica.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,17 +37,38 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @BindView(R.id.logIn)
     TextView mLogin;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
         ButterKnife.bind(this);
-
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+        
         mSignUpButton.setOnClickListener(this);
         mLogin.setOnClickListener(this);
 
     }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
     @Override
     public void onClick(View view) {
         if (view == mLogin) {
@@ -54,5 +83,37 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     }
 
     private void createNewUser() {
+        final String name = mUserName.getText().toString().trim();
+        final String email = mEmailId.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mConfrimPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication Successful.",
+                                    Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+  
 }
