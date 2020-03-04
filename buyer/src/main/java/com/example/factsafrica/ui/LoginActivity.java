d5d571e68e5894c4e0a7d5c7abfd3f02ba8429b3
ShadrackAdwera <adweshshaddie@gmail.com>
2020-03-04 14:32:57 +0300

    initial commit

--- buyer/src/main/java/com/example/factsafrica/ui/LoginActivity.java
+++ buyer/src/main/java/com/example/factsafrica/ui/LoginActivity.java
@@ -3,14 +3,35 @@ package com.example.factsafrica.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.factsafrica.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.mail) EditText mEd1;  //bind email text
+    @BindView(R.id.password) EditText mEd2;//bind password text
+
+
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
 }
