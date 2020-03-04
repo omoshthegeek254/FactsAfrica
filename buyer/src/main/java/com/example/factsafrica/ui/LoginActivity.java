b74c191e4029bc9f6bf925ac85ae68977ef5b5a8
FrankKamala <frankkamala@gmail.com>
2020-03-04 12:25:06 +0300

    implement onclick

--- buyer/src/main/java/com/example/factsafrica/ui/LoginActivity.java
+++ buyer/src/main/java/com/example/factsafrica/ui/LoginActivity.java
@@ -3,14 +3,20 @@ package com.example.factsafrica.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.factsafrica.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
