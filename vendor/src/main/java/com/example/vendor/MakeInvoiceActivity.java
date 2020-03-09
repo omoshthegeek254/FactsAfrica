633c0c0a11098786e61fafcd8a6e8fb8de601b48
FrankKamala <frankkamala@gmail.com>
2020-03-09 16:04:59 +0300

    dialog DATE

--- vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
+++ vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
@@ -2,6 +2,9 @@ package com.example.vendor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -25,6 +28,16 @@ public class MakeInvoiceActivity extends AppCompatActivity {
                 int month = myCal.get(Calendar.MONTH);
                 int day = myCal.get(Calendar.DAY_OF_WEEK);
 
+
+                //Date Picker
+
+                DatePickerDialog myDialog = DatePickerDialog(
+                        MakeInvoiceActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,year,month,day
+                );
+                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                myDialog.show();
             }
         });
 
