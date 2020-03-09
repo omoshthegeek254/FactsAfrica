02edab99c4c63426d5294f6bc9c771be0d0b31c3
FrankKamala <frankkamala@gmail.com>
2020-03-09 15:55:13 +0300

    add click listener to TextView

--- vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
+++ vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
@@ -3,12 +3,24 @@ package com.example.vendor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MakeInvoiceActivity extends AppCompatActivity {
+    private TextView mText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_invoice);
+        mText1= (TextView)findViewById(R.id.createDate);
+
+        mText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
