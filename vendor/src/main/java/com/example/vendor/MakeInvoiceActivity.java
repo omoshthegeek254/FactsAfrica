e5cec2cb0dfa8254f8e8095d074d777f90574eba
FrankKamala <frankkamala@gmail.com>
2020-03-09 16:26:38 +0300

    add EditText Display Date on EditText

--- vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
+++ vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
@@ -7,18 +7,23 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MakeInvoiceActivity extends AppCompatActivity {
-    private TextView mText1;
+
+
+    private EditText mText1;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_invoice);
-        mText1= (TextView)findViewById(R.id.createDate);
+        mText1= (EditText) findViewById(R.id.editTextDate);
 
         mText1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,19 +32,31 @@ public class MakeInvoiceActivity extends AppCompatActivity {
                 int year =myCal.get(Calendar.YEAR);
                 int month = myCal.get(Calendar.MONTH);
                 int day = myCal.get(Calendar.DAY_OF_WEEK);
+//Date Picker
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MakeInvoiceActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
 
 
-                //Date Picker
-
-                DatePickerDialog myDialog = DatePickerDialog(
-                        MakeInvoiceActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,year,month,day
-                );
-                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-                myDialog.show();
             }
         });
+
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+
+                String date = month  + "/" + day + "/" + year;
+
+                mText1.setText(date);
+
+            }
+        };
 
     }
 }
