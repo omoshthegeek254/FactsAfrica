f433979f9eca3fffa2f83596ef4d41212e7fb661
FrankKamala <frankkamala@gmail.com>
2020-03-10 11:45:24 +0300

    add click listener to All date pickers

--- vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
+++ vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
@@ -1,6 +1,7 @@
 package com.example.vendor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.DatePickerDialog;
 import android.graphics.Color;
@@ -13,6 +14,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 
@@ -30,13 +34,22 @@ public class MakeInvoiceActivity extends AppCompatActivity  implements View.OnCl
     @BindView(R.id.items) TextView mPickItems;
     @BindView(R.id.sendInvoice) Button mSend;
 
+
+
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private DatePickerDialog.OnDateSetListener mDateSetListener2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_invoice);
+
+
         ButterKnife.bind(this);
+        mInvoiceDate.setOnClickListener(this);
+        mDue.setOnClickListener(this);
+        mCustomer.setOnClickListener(this);
+
 
 //        mText1= (TextView) findViewById(R.id.editImageDate);
 
@@ -61,33 +74,75 @@ public class MakeInvoiceActivity extends AppCompatActivity  implements View.OnCl
 //        });
 
 
-//        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-//                month = month+1;
-//
-//                String date = month  + "/" + day + "/" + year;
-//
-//
-//                mText1.setText(date);
-//
-//            }
-//        };
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+
+                String date = month  + "/" + day + "/" + year;
+
+
+                mInvoiceDate.setText(date);
+
+
+            }
+        };
+        mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+
+                String date = month  + "/" + day + "/" + year;
+
+
+                mDue.setText(date);
+
+
+            }
+        };
 
     }
+    Calendar myCal = Calendar.getInstance();
 
+    int year =myCal.get(Calendar.YEAR);
+    int month = myCal.get(Calendar.MONTH);
+    int day = myCal.get(Calendar.DAY_OF_WEEK);
     @Override
     public void onClick(View v) {
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                MakeInvoiceActivity.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                mDateSetListener,
+                year,month,day);
         switch(v.getId()){
             case R.id.editImageDate:
                 /// Do this
 
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+
                 break;
 
             case R.id.payBy:
-                ////Do this
+
+                DatePickerDialog mDialog = new DatePickerDialog(
+                        MakeInvoiceActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener2,
+                        year,month,day);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                mDialog.show();
 
                 break;
+            case R.id.customer:
+                Snackbar.make(v, "Customer list incoming", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                break;
+
         }
 
     }
