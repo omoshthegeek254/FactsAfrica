10b5da6ea8fc6c188ed92228f6fdae1fec98ab72
FrankKamala <frankkamala@gmail.com>
2020-03-10 10:52:15 +0300

    switch to binding with butterknife

--- vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
+++ vendor/src/main/java/com/example/vendor/MakeInvoiceActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,51 +15,63 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MakeInvoiceActivity extends AppCompatActivity  {
 
 
     private TextView mText1;
+
+    @BindView(R.id.editImageDate) TextView mInvoiceDate;
+    @BindView(R.id.payBy) TextView mDue;
+    @BindView(R.id.customer) TextView mCustomer;
+    @BindView(R.id.items) TextView mPickItems;
+    @BindView(R.id.sendInvoice) Button mSend;
+
     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_invoice);
-        mText1= (TextView) findViewById(R.id.editImageDate);
+        ButterKnife.bind(this);
 
-        mText1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar myCal = Calendar.getInstance();
-                int year =myCal.get(Calendar.YEAR);
-                int month = myCal.get(Calendar.MONTH);
-                int day = myCal.get(Calendar.DAY_OF_WEEK);
-//Date Picker
-                DatePickerDialog dialog = new DatePickerDialog(
-                        MakeInvoiceActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
-                        year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+//        mText1= (TextView) findViewById(R.id.editImageDate);
 
-
-            }
-        });
-
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month+1;
-
-                String date = month  + "/" + day + "/" + year;
+//        mText1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Calendar myCal = Calendar.getInstance();
+//                int year =myCal.get(Calendar.YEAR);
+//                int month = myCal.get(Calendar.MONTH);
+//                int day = myCal.get(Calendar.DAY_OF_WEEK);
+////Date Picker
+//                DatePickerDialog dialog = new DatePickerDialog(
+//                        MakeInvoiceActivity.this,
+//                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+//                        mDateSetListener,
+//                        year,month,day);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+//
+//
+//            }
+//        });
 
 
-                mText1.setText(date);
-
-            }
-        };
+//        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//                month = month+1;
+//
+//                String date = month  + "/" + day + "/" + year;
+//
+//
+//                mText1.setText(date);
+//
+//            }
+//        };
 
     }
 }
