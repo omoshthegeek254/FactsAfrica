e4b3e29cf86926a303c9c265cbd20c0f85020211
FrankKamala <frankkamala@gmail.com>
2020-03-11 09:41:16 +0300

    Add VendorInvoice Adapter and Add Methods getFilterOnbindViewHolder CreateViewHolder and Get Item Count

---
+++ vendor/src/main/java/com/example/vendor/adapters/VendorInvoiceAdapter.java
@@ -0,0 +1,31 @@
+package com.example.vendor.adapters;
+
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class VendorInvoiceAdapter extends RecyclerView.Adapter<VendorInvoiceAdapter.VendorInvoiceViewHolder> implements Filterable {
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public VendorInvoiceAdapter.VendorInvoiceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VendorInvoiceAdapter.VendorInvoiceViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
