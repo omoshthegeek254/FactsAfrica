c87ed59c5ef607e2bbf01c52bded1965fb6fddb4
FrankKamala <frankkamala@gmail.com>
2020-03-11 13:15:15 +0300

    add get INvoice on homeFRag

--- vendor/src/main/java/com/example/vendor/ui/ui/home/HomeFragment.java
+++ vendor/src/main/java/com/example/vendor/ui/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.vendor.ui.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vendor.MakeInvoiceActivity;
 import com.example.vendor.R;
+import com.example.vendor.adapters.VendorInvoiceAdapter;
+import com.example.vendor.models.Invoice;
+import com.example.vendor.network.FactsAfricaApi;
+import com.example.vendor.network.FactsAfricaClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class HomeFragment extends Fragment {
@@ -45,6 +58,33 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+
+
+    //Get Invoices
+    public void getAllInvoices() {
+
+        FactsAfricaApi service = FactsAfricaClient.getClient().create(FactsAfricaApi.class);
+        Call<List<Invoice>> call = service.getAllInvoices();
+        Log.v("URL", call.request().url().toString());
+        call.enqueue(new Callback<List<Invoice>>() {
+            @Override
+            public void onResponse(Call<List<Invoice>> call, Response<List<Invoice>> response) {
+                invoices = response.body();
+                VendorInvoiceAdapter adapter = new VendorInvoiceAdapter(invoices, rootView.getContext());
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(rootView.getContext(), LinearLayoutManager.VERTICAL, false);
+                mInvoicesRecycler.setLayoutManager(layoutManager);
+                mInvoicesRecycler.setHasFixedSize(true);
+                mInvoicesRecycler.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Invoice>> call, Throwable t) {
+
+            }
+        });
     }
 
 
