82c7d0a73ba1e28bc915212a9e88ed0c62f897fa
FrankKamala <frankkamala@gmail.com>
2020-03-23 11:45:26 +0300

    add Post to FactsAPI interface

--- buyer/src/main/java/com/example/factsafrica/ui/network/FactsAfricaApi.java
+++ buyer/src/main/java/com/example/factsafrica/ui/network/FactsAfricaApi.java
@@ -6,11 +6,18 @@ import com.example.factsafrica.ui.models.PurchaseOrder;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface FactsAfricaApi {
     @GET("invoices")
     Call<List<Invoice>> getAllInvoices();
     @GET("purchase_orders")
     Call<List<PurchaseOrder>> getAllOrders();
+    //post
+    @POST("invoice/update/{id}")
+    Call<List<Invoice>>setReviewed(@Header("Authorization") String bearerToken,
+                                   @Body Invoice invoice);
 }
