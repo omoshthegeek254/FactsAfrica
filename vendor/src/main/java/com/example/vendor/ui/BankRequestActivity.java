48a7ef8a4a42c3aa9684ef9959bfc33d9652d6bf
ShadrackAdwera <adweshshaddie@gmail.com>
2020-03-24 12:11:36 +0300

    create make request to the bank activity

---
+++ vendor/src/main/java/com/example/vendor/ui/BankRequestActivity.java
@@ -0,0 +1,16 @@
+package com.example.vendor.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vendor.R;
+
+public class BankRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_request);
+    }
+}
