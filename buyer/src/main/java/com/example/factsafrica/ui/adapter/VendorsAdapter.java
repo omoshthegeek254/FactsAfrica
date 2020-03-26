959b3608b8bb6f1bfb02e88f2833542d4840e5f6
FrankKamala <frankkamala@gmail.com>
2020-03-26 10:02:42 +0300

    add constructor and methods

--- buyer/src/main/java/com/example/factsafrica/ui/adapter/VendorsAdapter.java
+++ buyer/src/main/java/com/example/factsafrica/ui/adapter/VendorsAdapter.java
@@ -1,4 +1,38 @@
 package com.example.factsafrica.ui.adapter;
 
-public class VendorsAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.factsafrica.ui.models.User;
+
+import java.util.List;
+
+public class VendorsAdapter extends RecyclerView.Adapter<VendorsAdapter.VendorViewHolder> {
+    private List<User> vendors;
+    private Context mContext;
+
+    public VendorsAdapter(List<User> vendors,Context mContext){
+        this.vendors = vendors;
+        this.mContext= mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public VendorsAdapter.VendorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VendorsAdapter.VendorViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
